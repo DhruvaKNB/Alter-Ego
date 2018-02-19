c53a13b98520271768091ace6131b34ee3efff7b
DhruvaKNB <dnkbhagdikar@hotmail.com>
2018-02-19 22:19:50 +0530

    Integrating Servers and changed front page

--- server.js
+++ server.js
@@ -34,6 +34,29 @@ app.listen(port,function(){
   connection.connect();
   console.log("Connected to database");
 });
+app.post('/login/psych',passport.authenticate('local2'),function(req,res){
+  console.log(req.body);
+  res.redirect('/index');
+});
+
+passport.use('local2',new passportLocal(function(username,password,done){
+    console.log("1");
+    connection.query(`select* from psychiatrists where userid='${username}'`,function(err,result,fields){
+    console.log("2");
+    if(result.length){
+        console.log("3");
+        bcrypt.compare(password,result[0].pwd,function(err,res){
+          console.log(res);
+          if(res)
+            return done(null,{user:false,id:result[0].userid},{message:"logged in succesfully"});
+          else
+            return done(null,false,{message:"username/password is incorrect"});
+        });
+      }
+    else
+      return done(null,false,{message:"username/password is incorrect"});
+  });
+}));
 app.post("/signup/psych",function(req,res){
   console.log("came here");
   var rounds=2;
@@ -41,11 +64,11 @@ app.post("/signup/psych",function(req,res){
     let query=`insert into ?? values(?,?,?,?,?)`;
     connection.query(sql.format(query,['psychiatrists',req.body.fname,req.body.lname,req.body.userid,data,req.body.regno]),function(err,data){
       if(err){
-        res.sendStatus(404);
+        res.render('home.ejs');
         console.log(err);
       }
       else
-        res.sendStatus(200);
+        res.render('home.ejs');
     });
   });
 });
@@ -56,11 +79,11 @@ app.post("/signup/user",function(req,res){
     let query=`insert into ?? values(?,?,?,?,?)`;
     connection.query(sql.format(query,['users',req.body.fname,req.body.lname,req.body.userid,data,req.body.age]),function(err,data){
       if(err){
-        res.sendStatus(404);
+        res.render('home.ejs');
         console.log(err);
       }
       else
-        res.sendStatus(200);
+        res.render('home.ejs');
     });
   });
 });
@@ -72,12 +95,12 @@ app.post('/login/user',passport.authenticate('local'),function(req,res){
   console.log(req.body);
   res.redirect('/index');
 });
-passport.use(new passportLocal(function(username,password,done){
+passport.use('local',new passportLocal(function(username,password,done){
     connection.query(`select* from users where userid='${username}'`,function(err,result,fields){
     if(result.length){
         bcrypt.compare(password,result[0].pwd,function(err,res){
           if(res)
-            return done(null,result[0].userid,{message:"logged in succesfully"});
+            return done(null,{user:true,id:result[0].userid},{message:"logged in succesfully"});
           else
             return done(null,false,{message:"username/password is incorrect"});
         });
@@ -93,7 +116,8 @@ passport.deserializeUser(function(id,done){
   //WHAT IS RETUNRED FROM HERE IS SET AS REQ.USER IN THE /SUCCESS
   //if(id===userconfig.id)
     var isd={};
-    connection.query(`select* from users where userid='${id}'`,function(err,result,fields){
+    if(id.user){
+    connection.query(`select* from users where userid='${id.id}'`,function(err,result,fields){
       if(result.length){
         isd.fname=result[0].fname;
         isd.lname=result[0].lname;
@@ -102,15 +126,19 @@ passport.deserializeUser(function(id,done){
         return done(null,isd);
       }
     });
-});
-app.get('/user/success',function(req,res){
-  console.log(req.user);
-  res.redirect('/index');
-});
-app.get('/user/failure',function(req,res){
-  console.log("failure");
-  res.render("home.ejs");
-});
+  }
+    else{
+      connection.query(`select* from psychiatrists where userid='${id.id}'`,function(err,result,fields){
+        if(result.length){
+          isd.fname=result[0].fname;
+          isd.lname=result[0].lname;
+          isd.userid=result[0].userid;
+          isd.regno=result[0].regno;
+          return done(null,isd);
+        }
+    });
+    }
+} );
 app.post('/user/send',function(req,res){
   console.log("hi");
   connection.beginTransaction(function(err){
@@ -151,11 +179,51 @@ app.post('/user/fetch',function(req,res){
     });
   });
 });
+app.post('/psych/send',function(req,res){
+  console.log("hi");
+  connection.beginTransaction(function(err){
+    if(err) console.log(err);
+    let query='insert into ?? values(?,?,?,?)';
+    connection.query(sql.format(query,['mp',0,req.body.source,req.body.dest,req.body.content]),function(err,data){
+      if(err){console.log(err);connection.rollback(function(){console.log(err);});}
+      else connection.commit(function(err){
+          if(err){console.log(err);connection.rollback(function(){console.log(err);});}
+          else console.log("sent");
+          res.sendStatus(200);
+      });
+    });
+  });
+});
+app.post('/psych/fetch',function(req,res){
+  console.log("hi");
+  connection.beginTransaction(function(err){
+    if(err) {res.sendStatus(404);console.log(err);}
+    let query = 'select ??,?? from ?? where ??=? order by ??';
+    connection.query(sql.format(query,['source','content','mu','destination',req.body.uid,'source']),function(err,data,fields){
+      if(err){console.log(err);connection.rollback(function(){res.sendStatus(404);console.log(err);});}
+      else connection.commit(function(err){
+        if(err){console.log(err);connection.rollback(function(){res.sendStatus(404);console.log(err);})}
+        console.log(data);
+        connection.beginTransaction(function(err){
+          if(err){res.sendStatus(404);console.log(err);}
+          let query='delete from ?? where ??=?';
+          connection.query(sql.format(query,['mu','destination',req.body.uid]),function(err,data2,field){
+            if(err){console.log(err);connection.rollback(function(){res.sendStatus(404);console.log(err);});}
+            else connection.commit(function(err){
+              if(err){connection.rollback(function(){console.log(err)});}
+              else {res.send(data);}
+            });
+          });
+        });
+      });
+    });
+  });
+});
 app.get('/user/logout',function(req,res){
   req.logout();
   res.render('home.ejs');
 });
-app.get('/home',function(req,res){
+app.get('/',function(req,res){
   res.render('home.ejs');
 })
 app.get('/index',function(req,res){
